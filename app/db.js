1b644e8366fc98f933b907c6674d886f9c62aedd
Marco Quintero <mquintero50@stuy.edu>
2025-05-09 11:47:15 -0400

    End of class

--- app/db.js
+++ app/db.js
@@ -1,12 +1,36 @@
-const createTable(){
-  let db = sqlite3.connect(DB_FILE, check_same_thread=False)
-  c = db.cursor()
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./mydatabase.db', (err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.message);
+  } else {
+    console.log('Connected to the database');
+  }
+});
+var DB_FILE = "dwitter.db";
+
+function createTable(){
+  let db = sqlite3.connect(DB_FILE, check_same_thread=False);
+  let c = db.cursor();
   c.execute('''
     CREATE TABLE IF NOT EXISTS userData (
       userID INTEGER PRIMARY KEY,
       username TEXT UNIQUE NOT NULL,
       password TEXT NOT NULL)
-    ''')
-  db.commit()
-  db.close()
-}
+    ''');
+  this.db.commit();
+  this.db.close();
+};
+
+function addUser(username, password){
+  let db = sqlite3.connect(DB_FILE, check_same_thread=False);
+  let c = db.cursor();
+  c.execute("SELECT userID FROM userData;");
+  let num = c.fetchall();
+  c.execute("INSERT INTO userData (userID, username, password) VALUES (?, ?, ?)", (Object.keys(num).length, username, password));
+
+  this.db.commit()
+  this.db.close()
+};
+
+let output = createTable();
+console.log(output);
