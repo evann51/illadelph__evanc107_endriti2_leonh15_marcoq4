0b78b7c718ee24abe37a441fd56d9c1a53481eb5
evan <echan51@stuy.edu>
2025-05-11 22:59:00 -0400

    nodemmigration

---
+++ app/index.js
@@ -0,0 +1,29 @@
+//setup
+const express = require('express');
+const app = express();
+
+//template work
+const path = require('path');
+app.set('view engine', 'ejs');
+
+const PORT = 5000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get('/', (req, res) => {
+  const data = { //change later to whatecver data we need
+  };
+  res.render('home', data);
+});
+
+app.get('/inbox', (req, res) => {
+  const data = {
+
+  };
+})
+
+app.listen(PORT, () => {
+  console.log(`running at http://localhost:${PORT}`);
+});
