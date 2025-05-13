104488f6d4718332cfa4b7eea2f6b50f410863d9
Marco Quintero <mquintero50@stuy.edu>
2025-05-13 11:36:37 -0400

    Got db.js to actually run. Throwing SQLITE_ERROR no such table: userData

--- app/db.js
+++ app/db.js
@@ -1,36 +1,54 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./mydatabase.db', (err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err.message);
-  } else {
-    console.log('Connected to the database');
-  }
-});
-var DB_FILE = "dwitter.db";
 
 function createTable(){
-  let db = sqlite3.connect(DB_FILE, check_same_thread=False);
-  let c = db.cursor();
-  c.execute('''
-    CREATE TABLE IF NOT EXISTS userData (
-      userID INTEGER PRIMARY KEY,
-      username TEXT UNIQUE NOT NULL,
-      password TEXT NOT NULL)
-    ''');
-  this.db.commit();
-  this.db.close();
+  const db = new sqlite3.Database('./dwitter.db', (err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err.message);
+    } else {
+      console.log('Connected to the database');
+    }
+  });
+  db.run("CREATE TABLE IF NOT EXISTS userData (userID INTEGER PRIMARY KEY, username TEXT UNIQUE NOT NULL, password TEXT NOT NULL)");
+  db.close();
 };
 
 function addUser(username, password){
-  let db = sqlite3.connect(DB_FILE, check_same_thread=False);
-  let c = db.cursor();
-  c.execute("SELECT userID FROM userData;");
-  let num = c.fetchall();
-  c.execute("INSERT INTO userData (userID, username, password) VALUES (?, ?, ?)", (Object.keys(num).length, username, password));
-
-  this.db.commit()
-  this.db.close()
+  const db = new sqlite3.Database('./dwitter.db', (err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err.message);
+    } else {
+      console.log('Connected to the database');
+    }
+  });
+  var num = [];
+  db.all("SELECT userID FROM userData;", (err, column) => {
+    if (err) {
+    console.error(err.message);
+    }
+    num = column;
+    console.log("All columns:" + num);
+  });
+  db.run("INSERT INTO userData (userID, username, password) VALUES (?, ?, ?)", (Object.keys(num).length, username, password));
+  db.close()
 };
 
-let output = createTable();
-console.log(output);
+function allUserData(){
+  const db = new sqlite3.Database('./dwitter.db', (err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err.message);
+    } else {
+      console.log('Connected to the database');
+    }
+  });
+  db.all("SELECT * FROM userData", (err, rows) => {
+    if (err) {
+    console.error(err.message);
+    }
+    let allRows = rows;
+    console.log("All rows:", allRows)
+  });
+}
+
+createTable();
+addUser("Tyson", "Brandt");
+allUserData()
