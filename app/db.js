3512b5eef60c29b98a2a1fe9cd0921de132bb4f2
Marco Quintero <mquintero50@stuy.edu>
2025-05-15 11:32:37 -0400

    createTable, addUser, and getUserData done

--- app/db.js
+++ app/db.js
@@ -7,9 +7,9 @@ function createTable(){
     } else {
       console.log('Connected to the database');
     }
+    db.run("CREATE TABLE IF NOT EXISTS userData (userID INTEGER PRIMARY KEY, username TEXT UNIQUE NOT NULL, password TEXT NOT NULL)");
+    db.close();
   });
-  db.run("CREATE TABLE IF NOT EXISTS userData (userID INTEGER PRIMARY KEY, username TEXT UNIQUE NOT NULL, password TEXT NOT NULL)");
-  db.close();
 };
 
 function addUser(username, password){
@@ -19,26 +19,26 @@ function addUser(username, password){
     } else {
       console.log('Connected to the database');
     }
-  });
-  let num = 0;
+    let num = 0;
 
-  db.all("SELECT userID FROM userData;", (err, column) => {
-    if (err) {
-    console.error(err.message);
-    }
-    if (column != null) {
-      num = Object.keys(column).length;
-    }
-    //console.log("Column: " + Object.keys(column).length);
-    //console.log("Variable num: " + num);
-    db.run("INSERT INTO userData (userID, username, password) VALUES (?, ?, ?)", [num, username, password], function(err) {
+    db.all("SELECT userID FROM userData;", (err, column) => {
       if (err) {
-        return console.error(err.message);
+      console.error(err.message);
       }
-      console.log("A row has been inserted with rowid ${this.lastID}");
+      if (column != null) {
+        num = Object.keys(column).length;
+      }
+      //console.log("Column: " + Object.keys(column).length);
+      //console.log("Variable num: " + num);
+      db.run("INSERT INTO userData (userID, username, password) VALUES (?, ?, ?)", [num, username, password], function(err) {
+        if (err) {
+          return console.error(err.message);
+        }
+        console.log("A row has been inserted with rowid ${this.lastID}");
+      });
     });
+    db.close();
   });
-  db.close()
 };
 
 function allUserData(){
@@ -48,18 +48,18 @@ function allUserData(){
     } else {
       console.log('Connected to the database');
     }
+    db.all("SELECT * FROM userData", (err, rows) => {
+      if (err) {
+      console.error(err.message);
+      }
+      let allRows = rows;
+      console.log("All rows:", allRows)
+    });
+    db.close();
   });
-  db.all("SELECT * FROM userData", (err, rows) => {
-    if (err) {
-    console.error(err.message);
-    }
-    let allRows = rows;
-    console.log("All rows:", allRows)
-  });
-  db.close()
 }
 
 createTable();
-addUser("Tyson", "Brandt");
-addUser("Israel", "Armani")
-//allUserData();
+//addUser("Tyson", "Brandt");
+//addUser("Israel", "Armani");
+allUserData();
