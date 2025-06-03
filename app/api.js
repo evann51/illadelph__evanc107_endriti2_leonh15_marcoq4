7e6cee613320310d2b55f4135e3a4b9b0f4925aa
Marco Quintero <mquintero50@stuy.edu>
2025-06-03 11:47:25 -0400

    Initial machinations of api.js

---
+++ app/api.js
@@ -0,0 +1,13 @@
+const CharacterAI = require('node_characterai');
+const characterAI = new CharacterAI();
+
+(async() => {
+    await characterAI.authenticateAsGuest();
+
+    const characterId = "8_1NyR8w1dOXmI1uWaieQcd147hecbdIK7CeEAIrdJw" // Discord moderator
+
+    const chat = await characterAI.createOrContinueChat(characterId);
+    const response = await chat.sendAndAwaitResponse('Hello discord mod!', true)
+
+    console.log(response);
+})();
