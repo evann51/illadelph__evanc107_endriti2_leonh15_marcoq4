079622fbfc1cb8b1dbecfd7c6e35eb3788fc5526
Marco Quintero <mquintero50@stuy.edu>
2025-06-05 21:31:42 -0400

    Added character options to the API

--- app/api.js
+++ app/api.js
@@ -1,12 +1,25 @@
 const { GoogleGenAI } = require("@google/genai");
 const fs = require('fs');
 const api_key = fs.readFileSync("keys/Gemini_key.txt", "utf8")
+//console.log(api_key);
 const ai = new GoogleGenAI({ apiKey: api_key});
 
 async function main(character, text) {
   let desc = "";
   if (character == 0){
     desc = "Twitter user trying to make people mad";
+  } else if (character == 1){
+    desc = "Genshin Impact fan that gets easily offended";
+  } else if (character == 2){
+    desc = "Middle-aged tech bro that only speaks in tech-related dad jokes"
+  } else if (character == 3){
+    desc = "Guy who only speaks to spread the word of God"
+  } else if (character == 4){
+    desc = "Conspiracy theorist that thinks the government is purposefully trying to hide information"
+  } else if (character == 5){
+    desc = "anime weeb who thinks they're an actual anime character"
+  } else if (character == 6){
+    desc = "a movie nerd that only talks in movie references"
   }
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
@@ -16,4 +29,4 @@ async function main(character, text) {
   return(response.text)
 }
 
-//main(0, "");
+main(6, "LeBron James is the GOAT");
