0563a3f357af81e17541961ae6a56e6606e3bded
Marco Quintero <mquintero50@stuy.edu>
2025-06-05 20:44:45 -0400

    Remodeled api function so that it takes in arguments

--- app/api.js
+++ app/api.js
@@ -1,12 +1,17 @@
 const { GoogleGenAI } = require("@google/genai");
 
 const ai = new GoogleGenAI({ apiKey: "AIzaSyAizreIXJ-IQ7HeczrWTqYYqoh_HpCpknA" });
-async function main() {
+async function main(character, text) {
+  let desc = "";
+  if (character == 0){
+    desc = "Twitter user trying to make people mad";
+  }
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
-    contents: "Respond to this quote and everything I say as if you were a Twitter user trying to make people mad (do not give any explanations as to what you say. just respond): 'GSP is the GOAT of the UFC'",
+    contents: `Respond to this quote and everything I say as if you were a ${desc} (do not give any explanations as to what you say. just respond): '${text}'`,
   });
   console.log(response.text);
+  return(response.text)
 }
 
-main();
+//main(0, "GSP is the GOAT of the UFC");
